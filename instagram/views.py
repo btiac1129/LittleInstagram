5c41dd7dff29350102fcfae12cf486525305414b
btiac1129 <btiac1129@gmail.com>
2020-10-21 09:54:49 +0900

    easy-thumbnails 활용한 인스타그램 유저페이지 (포스트) 썸네일 처리 구현

--- instagram/views.py
+++ instagram/views.py
@@ -5,6 +5,14 @@ from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from .models import Post, Tag
 
+
+@login_required
+def index(request):
+    return render(request, "instagram/index.html", {
+
+    })
+    pass
+
 @login_required
 def post_new(request):
     if request.method == 'POST':
@@ -32,7 +40,9 @@ def post_detail(request, pk):
 def user_page(request, username):
     page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
     post_list = Post.objects.filter(author=page_user)
+    post_list_count = post_list.count() # 실제 데이터베이스에 count 쿼리를 던지게 된다. len(post_list)보다 빠르게 동작.    
     return render(request, 'instagram/user_page.html', {
         "page_user": page_user,
         "post_list": post_list,
+        "post_list_count": post_list_count,
     })
