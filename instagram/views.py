9a6cefaf48b8eeb543a6c43ba650564217416fc6
btiac1129 <btiac1129@gmail.com>
2020-10-27 10:45:10 +0900

    날짜 범위 지정, 포스팅 좋아요/취소 구현

--- instagram/views.py
+++ instagram/views.py
@@ -1,27 +1,32 @@
+from datetime import timedelta
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.utils import timezone
 from .forms import PostForm
 from .models import Post, Tag
 
 
 @login_required
 def index(request):
+    timesince = timezone.now() - timedelta(days=3)
     post_list = Post.objects.all()\
         .filter(
             Q(author=request.user) |
             Q(author__in=request.user.following_set.all()) 
+        )\
+        .filter(
+            created_at__gte = timesince
         )
-#        .filter(author__in=request.user.following_set.all())
-#   suggested_user_list = get_user_model().objects.all()\
-#        .exclude(pk=request.user.pk)\
-#        .exclude(pk__in=request.user.following_set.all())[:3]
+    suggested_user_list = get_user_model().objects.all()\
+        .exclude(pk=request.user.pk)\
+        .exclude(pk__in=request.user.following_set.all())[:3]
     
     return render(request, "instagram/index.html", {
         "post_list": post_list,
-#        "suggested_user_list": suggested_user_list,
+        "suggested_user_list": suggested_user_list,
     })
 
 @login_required
@@ -48,6 +53,27 @@ def post_detail(request, pk):
         "post": post,
     })
 
+@login_required
+def post_like(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # TODO: like 처리 필요
+    post.like_user_set.add(request.user)
+
+    messages.success(request, f"포스트#{post.pk}를 좋아합니다.")
+    redirect_url = request.META.get("HTTP_REFERER", "root")
+    return redirect(redirect_url)    
+
+@login_required
+def post_unlike(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # TODO: like 처리 필요
+    post.like_user_set.remove(request.user)
+
+    messages.success(request, f"포스트#{post.pk} 좋아요를 취소합니다.")
+    redirect_url = request.META.get("HTTP_REFERER", "root")
+    return redirect(redirect_url)    
+
+
 def user_page(request, username):
     page_user = get_object_or_404(get_user_model(), username=username, is_active=True)
     
@@ -58,8 +84,12 @@ def user_page(request, username):
     
     post_list = Post.objects.filter(author=page_user)
     post_list_count = post_list.count() # 실제 데이터베이스에 count 쿼리를 던지게 된다. len(post_list)보다 빠르게 동작.    
+    follower_set_count = page_user.follower_set.count()
+    following_set_count = page_user.following_set.count()
     return render(request, 'instagram/user_page.html', {
         "page_user": page_user,
+        "follower_set_count": follower_set_count,
+        "following_set_count": following_set_count,
         "post_list": post_list,
         "post_list_count": post_list_count,
         "is_follow": is_follow,
